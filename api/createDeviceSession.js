4a6a0f0c5083e9f40a563f643a9df8c4ec3b84a2
Dima Shirokov <dima.shirokoff@rapidapi.com>
2016-11-08 10:31:35 +0200

    createDeviceSession

--- api/createDeviceSession.js
+++ api/createDeviceSession.js
@@ -3,57 +3,57 @@ const request = require('request');
 
 module.exports = (req, res) => {
 
-    // rpt bug
-    req.body.args = _.clearArgs(req.body.args, false);
+	// rpt bug
+	req.body.args = _.clearArgs(req.body.args, false);
 
-    let { 
+	let { 
 
-        deviceId,
-        identifier, 
-        language, 
-        timezone, 
-        gameVersion, 
-        deviceOs, 
-        adId, 
-        sdk, 
-        tags, 
-        to="to" } = req.body.args;
+		deviceId,
+		identifier, 
+		language, 
+		timezone, 
+		gameVersion, 
+		deviceOs, 
+		adId, 
+		sdk, 
+		tags, 
+		to="to" } = req.body.args;
 
-    let r  = {
+	let r  = {
         callback     : "",
         contextWrites: {}
     };
 
-    if(!deviceId) {
-        _.echoBadEnd(r, to, res);
-        return;
-    }
+	if(!deviceId) {
+		_.echoBadEnd(r, to, res, 'deviceId');
+		return;
+	}
 
-    let bodyOptions = {
-        identifier: identifier,
-        language: language,
-        timezone: timezone,
-        game_version: gameVersion,
-        device_os: deviceOs,
-        ad_id: adId,
-        sdk: sdk,
-        tags: tags
-    };
+	let bodyOptions = {
+		identifier: identifier,
+		language: language,
+		timezone: timezone,
+		game_version: gameVersion,
+		device_os: deviceOs,
+		ad_id: adId,
+		sdk: sdk,
+		tags: tags
+	};
 
-    bodyOptions = _.clearArgs(bodyOptions, true);
+	bodyOptions = _.clearArgs(bodyOptions, true);
 
-    let options = {
-        method: 'POST',
-        url: `https://onesignal.com/api/v1/players/${deviceId}/on_session`, 
-        body: JSON.stringify(bodyOptions),
-        headers: {
-            'Content-Type':  `application/json`,
-        }
-    }
+	let options = {
+		method: 'POST',
+		url: `https://onesignal.com/api/v1/players/${deviceId}/on_session`, 
+		body: JSON.stringify(bodyOptions),
+		headers: {
+			'Content-Type':  `application/json`,
+		}
+	}
 
-    return request(options, (err, response, body) => {
-        if(!err && response.statusCode == 200) {
-            r.contextWrites[to] = JSON.parse(body);
+	return request(options, (err, response, body) => {
+		if(!err && response.statusCode == 200) {
+    		r.contextWrites[to] = JSON.parse(body);
             r.callback = 'success'; 
         } else {
             r.contextWrites[to] = JSON.parse(err || body);
@@ -61,6 +61,6 @@ module.exports = (req, res) => {
         }
 
         res.status(200).send(r);
-    })
-    //.auth(null, null, true, apiKey);
+	})
+	//.auth(null, null, true, apiKey);
 }
